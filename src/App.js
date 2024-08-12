66ce849dc8c42b19fdb114e9e18c10f6c92f2b52
protein2005 <alexeysokole@gmail.com>
2024-08-12 20:17:42 +0300

    setup project structure with styles and react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Усі піци</h2>
+          <div className="content__items">
+            <PizzaBlock title={'Пепероні Фреш з перцем'} price={300} />
+            <PizzaBlock title={'Сирна'} price={250} />
+            <PizzaBlock title={'Курча барбекю'} price={150} />
+            <PizzaBlock title={'Кисло-солодке курча'} price={450} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Усі</li>
+        <li>М'ясні</li>
+        <li>Вегетаріанська</li>
+        <li>Гриль</li>
+        <li>Гострі</li>
+        <li>Закриті</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
